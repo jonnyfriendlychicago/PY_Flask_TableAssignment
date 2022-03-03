76afc8c61e71621760b6f4e54c57f2275cc0087f
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-03-03 13:32:38 -0600

    firstWorkingDraft

--- server.py
+++ server.py
@@ -11,13 +11,16 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')
 def render_lists():
     # Soon enough, we'll get data from a database, but for now, we're hard coding data
-    student_info = [
-        {'name' : 'Michael', 'age' : 35},
+    
+    randomNumbers = [3,1,5]
+
+    StudentInfoList = [
+        {'name' : 'MichaelangeloDonatelloLeonardoRaphael', 'age' : 35},
         {'name' : 'John', 'age' : 30 },
         {'name' : 'Mark', 'age' : 25},
         {'name' : 'KB', 'age' : 27}
     ]
-    return render_template("index.html", random_numbers = [3,1,5], students = student_info)
+    return render_template("index.html", displayRandomNumbers = randomNumbers, displayStudentInfoList = StudentInfoList)
 
 
 
